1d4165c254d674730d0ea24b963b0119fdac8726
Maitreya Meshram <mm360x@gmail.com>
2019-02-13 13:25:47 -0500

    Creating login

--- Prototype1.cpp
+++ Prototype1.cpp
@@ -1,17 +1,63 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-
+#include <fstream>
+#define MAX_USERS 100
+//cd PC/Desktop/'NotesLabs 2019 Winter/CPS707_Project/CPS707_ByteCode/
 using namespace std;
+ifstream inFile;
+string user [MAX_USERS], type [MAX_USERS];
+int credit [MAX_USERS];
+int userNumber = 0;
 
-int main() {
+string login_checker(string username)
+{
+  int jk = 0;
+  while(jk < userNumber)
+  {
+    if(username == user[jk])
+    {
+      return username;
+    }
+    jk++;
+  }
+  return "invalid";
+}
 
- string input = "";
-
- // How to get a string/sentence with spaces
- cout << "Please enter a valid sentence (with spaces):\n>";
- getline(cin, input);
- cout << "You entered: " << input << endl << endl;
+int main() 
+{
+  string input = "", username = "", temp = "", str = "";
+  inFile.open("currentUserAccounts.txt");
+  cout << endl;
+  while (getline(inFile, temp))
+    {
+        user[userNumber] = temp.substr(0,7);
+        type[userNumber] = temp.substr(16,2);
+        credit[userNumber] = stoi(temp.substr(19,9));
+        //cout << "User number " << i << " is : " << user[i] << " with type " << type[i] << " and avaliable credits " << credit[i] << endl;
+        userNumber++;
+    }
+  cout << "welcome to the ticket selling service!" << endl;
+ //Testing the login command
+ cin >> input;
+ if( input == "login")
+ {
+    cout << "Enter a username" << endl;
+    cin >> input;
+    username = login_checker(input);
+    if(username == "invalid")
+    {
+      cout << "The user " << username << " does not exist!" << endl;
+    }
+    else
+    {
+      cout << "You are now logged on as \"" << username << "\"" << endl;
+    }
+ }
+ else
+ {
+   cout << "You need to login into your account first" << endl;
+ }
 
  // How to get a number.
  int myNumber = 0;
