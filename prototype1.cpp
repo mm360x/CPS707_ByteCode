86947f4ecf054a21a19705c0c68e8a6a2b5c9f3a
Maitreya Meshram <mm360x@gmail.com>
2019-02-18 16:27:08 -0500

    commit

--- prototype1.cpp
+++ prototype1.cpp
@@ -10,189 +10,87 @@ string user [MAX_USERS], type [MAX_USERS];
 int credit [MAX_USERS];
 int userNumber = 0;
 
-int login_checker(string username)//searches for if a username matches an existing username
+string login_checker(string username)//searches for if a username matches an existing username
 {
   int jk = 0;
   while(jk < userNumber)
   {
-	if(username == user[jk])
-	{
-		return jk;//returns where in the user array this user is
-	}
-	jk++;
+    if(username == user[jk])
+    {
+      return username;
+    }
+    jk++;
   }
-  return -1;
-}
-
-string logout()
-{
-
-}
-string create() //allows an admin to create a new user
-{
-	string newuser, newusertype, money;
-
-	cout << "Enter the new users name: "
-	cin >> newuser
-	if (login_checker(newuser) != -1) {
-		cout << "that user already exits \n"
-		return ""
-	}
-	if (newuser.length() > 15)
-	{
-		cout << "The name " << newuser << " exceeds the maximum number of characters for a username, user creation cancelled";
-		return "";
-	}
-
-	cout << "Enter type: "
-	cin >> newusertype;
-	if (newusertype != "FS" || newusertype != "SS" || newusertype != "BS" || newusertype != "AA") {
-		cout << "that type does not exist";
-		return "";
-	}
-
-	cout << "Enter credit: "
-	cin >> money;
-	// This code converts from string to number safely.
-	int newusercredits;
-	stringstream myStream(money);
-	if (myStream >> newusercredits)
-	else
-	{
-		cout << "Invalid number" << endl;
-		return "";
-	}
-
-
-	int remainingspace = newuser.length() - 15;
-	string emptyspaces = "";
-	for (i = 0; i < remainingspace; i++) emptyspaces += '_';
-
-	string usersinfo = "00_" + newusername + emptyspaces + newusertype + newusercredits;
-	return "done";
-}
-
-string refund() //allows an admin to refund a transaction 
-{
-	cout << "in refund \n";
-	return "done";
-}
-
-string deleteuser() //allows an admin to delete an old user
-{
-	cout << "in delete\n";
-	return "done";
-}
-string addcredit() //allows any user to addcredit to their account
-{
-	cout << "in addcredit\n";
-	return "done";
-}
-string addcreditadmin() //allows an admin to addcredit to any account
-{
-	cout << "in addcreditadmin\n";
-	return "done";
-}
-string sell() //allows a seller to post offers
-{
-	cout << "in sell\n";
-	return "done";
-}
-
-string buy() //allows a buyer to buy tickets
-{
-	cout << "in sell\n";
-	return "done";
+  return "invalid";
 }
 
 int main() 
 {
-	string input = "", username = "", temp = "", str = "";
-	inFile.open("currentUserAccounts.txt");
+  string input = "", username = "", temp = "", str = "";
+  inFile.open("currentUserAccounts.txt");
+  cout << endl;
+  while (getline(inFile, temp))
+    {
+        user[userNumber] = temp.substr(0,7);
+        type[userNumber] = temp.substr(16,2);
+        credit[userNumber] = stoi(temp.substr(19,9));
+        //cout << "User number " << i << " is : " << user[i] << " with type " << type[i] << " and avaliable credits " << credit[i] << endl;
+        userNumber++;
+    }
+  cout << "welcome to the ticket selling service!" << endl;
+ //Testing the login command
+ cin >> input;
+ if( input == "login")
+ {
+    cout << "Enter a username" << endl;
+    cin >> input;
+    username = login_checker(input);
+    if(username == "invalid")
+    {
+      cout << "The user " << username << " does not exist!" << endl;
+    }
+    else
+    {
+      cout << "You are now logged on as \"" << username << "\"" << endl;
+    }
+ }
+ else
+ {
+   cout << "You need to login into your account first" << endl;
+ }
 
-	bool loggedin = false; //remembers if the user is already logged in
+ // How to get a number.
+ int myNumber = 0;
+/*
+ while (true) {
+   cout << "Please enter a valid number: ";
+   getline(cin, input);
 
-	cout << endl;
-	while (getline(inFile, temp))
-	  {
-	      user[userNumber] = temp.substr(0,7);
-	      type[userNumber] = temp.substr(16,2);
-	      credit[userNumber] = stoi(temp.substr(19,9));
-	      //cout << "User number " << i << " is : " << user[i] << " with type " << type[i] << " and avaliable credits " << credit[i] << endl;
-	      userNumber++;
-	  }
+   // This code converts from string to number safely.
+   stringstream myStream(input);
+   if (myStream >> myNumber)
+     break;
+   cout << "Invalid number, please try again" << endl;
+ }
+ cout << "You entered: " << myNumber << endl << endl;
 
-	cout << "welcome to the ticket selling service!" << endl;
-	while (true)
-	{
-		int userid = 0;
-		cin >> input;
-		//Testing the login command
-		if (input == "login")
-		{
-			if (loggedin)
-			{
-				cout << "You are already logged in! \n"
-					break;
-			}
-			cout << "Enter a username" << endl;
-			cin >> input;
-			userid = login_checker(input);
-			if (userid == -1)
-			{
-				cout << "The user " << username << " does not exist!" << endl;
-			}
-			else
-			{
-				username = input;
-				cout << "You are now logged on as \"" << username << "\"" << endl;
-				loggedin = true;
-				if(type[useridnumber] = "AA")
-				{
-					cout << "Welcome back " << username << " you now have access to special permissions"
-				}
-			}
-		}
-		else
-		{
-			cout << "You need to login into your account first" << endl;
-		}
+ // How to get a single char.
+ char myChar  = {0};
 
-		while (loggedin) //after logged in you can use other commands
-		{
-			cin >> input;
-			//Testing the logout command
-			if (input == "logout")
-			{
-				logout();
-				loggedin = false;
-				cout << "logged out\n"
-				cout << "welcome to the ticket selling service!\n"
-			}
-			else if (input == "create")
-				if(type[userid] = "AA")
-					create();
-			else if (input == "addcredit")
-				if (type[userid] = "AA")
-					addcreditadmin();
-				else addcredit();
-			else if (input == "delete")
-				if (admin)
-					deleteuser();
-			else if (input == "sell")
-				if(type[userid] == "FS" || type[userid] == "SS")
-					sell();
-				else cout << "you must have a";
-			else if (input == "buy")
-				if (type[userid] == "FS" || type[userid] == "BS")
-					buy();
-				else cout << "you must have a";
-			else if (input == "refund")
-				if (type[userid] == "AA")
-					refund();
-			else
-				cout << "invalid command";
-		}
-	}
+ while (true) {
+   cout << "Please enter 1 char: ";
+   getline(cin, input);
+
+   if (input.length() == 1) {
+     myChar = input[0];
+     break;
+   }
+
+   cout << "Invalid character, please try again" << endl;
+ }
+ cout << "You entered: " << myChar << endl << endl;
+*/
+ cout << "All done. And without using the >> operator" << endl;
+
  return 0;
 }
